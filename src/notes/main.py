b32a9226b081d1825c0d1071d5af5c093e938011
Richard Brearton <richard.brearton@racingbulls.com>
2024-10-23 19:47:11 +0100

    Added a simple main file

---
+++ src/notes/main.py
@@ -0,0 +1,13 @@
+"""
+This script is the entrypoint for the notes website.
+"""
+
+
+def main():
+    """
+    Running this function spins up the website.
+    """
+
+
+if __name__ == "__main__":
+    main()
