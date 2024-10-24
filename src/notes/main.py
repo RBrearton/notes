76a00ca3276119226d677fdb9a44b0fa2d86d371
Richard Brearton <richard.brearton@racingbulls.com>
2024-10-25 00:08:54 +0100

    Some updates to the main page

--- src/notes/main.py
+++ src/notes/main.py
@@ -4,59 +4,73 @@ This script is the entrypoint for the notes website.
 
 from nicegui import ui
 
-
 title = "Notes"
 
 
-@ui.page("/")
+@ui.page("/", favicon="")
 def main_page():
     """
     This function defines the content of the main landing page of the notes
     website.
     """
-    ui.markdown(
-        f"""
-# {title}
+    ui.colors(primary="teal")
+    ui.page_title(title)
 
-I'm going to try to put some teaching notes for the B2 and B6 courses here.
-I'll update this throughout term.
+    ui.markdown(f"# {title}")
+    ui.label(
+        """
+        I'm going to try to put some teaching notes for the B2 and B6 courses
+        here.
+        I'll update this throughout term.
         """
     )
 
-    # Add some options to the right drawer.
-    with ui.right_drawer(fixed=False, bordered=False).props(
-        "bordered"
-    ) as right_drawer:
-        ui.markdown("# :)")
-        right_drawer.toggle()
+    # The right drawer.
+    with ui.right_drawer(fixed=False):
+        ui.markdown("This is another menu drawer.")
 
     # Add the header.
-    with ui.header(elevated=True).classes("items-center justify-between"):
-        ui.markdown(f"##{title}")
-        ui.button(on_click=right_drawer.toggle, icon="menu").props(
-            "flat color=white"
-        )
+    with (
+        ui.header(elevated=True, bordered=True)
+        .props('height="h-1"')
+        .classes("items-center justify-between")
+    ):
+        # These go on the left.
+        with ui.row().classes("items-center"):
+            ui.icon("description", size="md")
+            ui.label("Notes").classes("text-2xl font-bold")
 
-    # Make a left drawer that's the same width as we would have with material
-    # mkdocs.
-    with ui.left_drawer(bordered=False):
+        # These go on the right.
+        with ui.row().classes("items-center"):
+            ui.input(placeholder="Search...").props(
+                'input-style="color: white"'
+            )
+            ui.button(icon="menu").props("flat color=white")
+
+    # The left drawer.
+    with ui.left_drawer(fixed=False):
         ui.markdown(f"##### {title}")
-        with ui.list():
-            ui.item("B6").props("clickable")
-            ui.item("PS1")
-            ui.item("PS2")
-            ui.item("PS3")
-            ui.item("PS4")
-            ui.item("PS5")
-            with ui.item("B2").props("clickable"):
-                ui.item_section("PS1")
-                ui.item_section("PS2")
-                ui.item_section("PS3")
-                ui.item_section("PS4")
-                ui.item_section("PS5")
+        with ui.column(align_items="stretch").style("gap: 0"):
+            with (
+                ui.expansion("B2: Symmetry and relativity")
+                .props("dense dense-toggle")
+                .classes("non-selectable")
+            ):
+                with ui.column().style("gap: 0"):
+                    ui.label("PS1")
+                    ui.label("PS2")
 
-    # Make a footer that links to the github issues page.
-    with ui.footer():
+            with (
+                ui.expansion("B6: Condensed matter physics")
+                .props("dense dense-toggle")
+                .classes("non-selectable")
+            ).style("gap: 0"):
+                ui.label("PS1")
+                ui.label("PS2")
+
+    # The footer.
+    with ui.footer(fixed=False):
+        # Put a link to the github issues page.
         ui.label(
             "Spot any mistakes? Go to the",
         )
@@ -71,4 +85,4 @@ def main():
     """
 
 
-ui.run()
+ui.run(favicon="icon.png")
